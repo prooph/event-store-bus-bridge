ddf560e22750b772afedfc1f5b88ba89cbc81d1d
prolic <saschaprolic@googlemail.com>
2016-10-07 16:50:17 +0800

    add docheader and update copyright

--- src/Container/TransactionManagerFactory.php
+++ src/Container/TransactionManagerFactory.php
@@ -1,12 +1,11 @@
 <?php
-/*
- * This file is part of the prooph/event-store-bus-bridge.
- * (c) 2014-2015 prooph software GmbH <contact@prooph.de>
+/**
+ * This file is part of the prooph/service-bus.
+ * (c) 2014-%year% prooph software GmbH <contact@prooph.de>
+ * (c) 2015-%year% Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * Date: 8/30/15 - 2:14 PM
  */
 namespace Prooph\EventStoreBusBridge\Container;
 
