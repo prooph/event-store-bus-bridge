7a611adf4a647f6facf9f8d9cf5033457f95ab15
Bas Kamer <bas@bushbaby.nl>
2018-11-02 14:23:05 +0100

    apply prooph cs-fixer

--- src/Container/TransactionManagerFactory.php
+++ src/Container/TransactionManagerFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the prooph/event-store-bus-bridge.
  * (c) 2014-2018 prooph software GmbH <contact@prooph.de>
@@ -7,6 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 declare(strict_types=1);
 
 namespace Prooph\EventStoreBusBridge\Container;
